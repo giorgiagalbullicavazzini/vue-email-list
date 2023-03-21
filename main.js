7c4b6c8fb64549269b54d5615465deb014f2d527
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-21 15:19:08 +0100

    generation of random emails

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 const { createApp } = Vue;
 
+const app = createApp({
+    data() {
+        return {
+            // Number of emails to generate
+            times: 10,
+            // Emails array
+            emails: [],
+        }
+    },
+    beforeMount() {
+        // Recalling an API to get the correct number of random emails
+        for (let i = 1; i <= this.times; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const email = response.data.response;
+                console.log(email);
+                this.emails.push(email);
+            })
+        }
+    }
+}).mount('#app');
